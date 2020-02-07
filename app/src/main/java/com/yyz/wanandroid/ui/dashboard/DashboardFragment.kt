25784122a74f50aecd98ea7ae7ddd5e35a46bf10
Young Yunzhao <fly_with24@163.com>
2020-02-07 12:18:27 +0800

    优化viewModel初始化逻辑

--- app/src/main/java/com/yyz/wanandroid/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/yyz/wanandroid/ui/dashboard/DashboardFragment.kt
@@ -6,21 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.yyz.wanandroid.R
 
 class DashboardFragment : Fragment() {
-
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private val dashboardViewModel by viewModels<DashboardViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
