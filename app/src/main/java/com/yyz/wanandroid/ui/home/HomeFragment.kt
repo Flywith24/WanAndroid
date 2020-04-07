e475b0049c7c8b48aa430ed3384e7e587d3332a0
fly_with24 <fly_with24@163.com>
2020-04-07 09:47:39 +0800

    使用 ViewPager2

--- app/src/main/java/com/yyz/wanandroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/yyz/wanandroid/ui/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.yyz.wanandroid.ui.home
 
 import android.view.View
 import androidx.fragment.app.viewModels
+import com.google.android.material.tabs.TabLayoutMediator
 import com.yyz.wanandroid.R
 import com.yyz.wanandroid.base.BaseFragment
 import com.yyz.wanandroid.databinding.FragmentHomeBinding
@@ -13,8 +14,16 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun initBinding(view: View): FragmentHomeBinding = FragmentHomeBinding.bind(view)
 
     override fun initData() {
-        binding.viewPager.adapter = SectionsPagerAdapter(parentFragmentManager)
-        binding.tabs.setupWithViewPager(binding.viewPager)
+        binding.viewPager.adapter = SectionsPagerAdapter(this)
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = TAB_TITLES[position]
+        }.attach()
     }
 
+    companion object {
+        private val TAB_TITLES = arrayOf(
+            "最新博文",
+            "最新项目"
+        )
+    }
 }
--- app/src/main/java/com/yyz/wanandroid/ui/home/SectionsPagerAdapter.kt
+++ app/src/main/java/com/yyz/wanandroid/ui/home/SectionsPagerAdapter.kt
@@ -1,15 +1,9 @@
 package com.yyz.wanandroid.ui.home
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.yyz.wanandroid.ui.home.tab.ArticleListFragment
 import com.yyz.wanandroid.ui.home.tab.ProjectListFragment
-
-private val TAB_TITLES = arrayOf(
-    "最新博文",
-    "最新项目"
-)
 
 /**
  * @author yyz (杨云召)
@@ -17,19 +11,11 @@ private val TAB_TITLES = arrayOf(
  * time   22:31
  * description
  */
-class SectionsPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(
-    fm,
-    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
-) {
-    private val list: List<Fragment> = listOf(ArticleListFragment(), ProjectListFragment())
+class SectionsPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = 2
 
-    override fun getItem(position: Int): Fragment {
-        return list[position]
+    override fun createFragment(position: Int): Fragment = when (position) {
+        1 -> ProjectListFragment()
+        else -> ArticleListFragment()
     }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return TAB_TITLES[position]
-    }
-
-    override fun getCount(): Int = TAB_TITLES.size
 }
