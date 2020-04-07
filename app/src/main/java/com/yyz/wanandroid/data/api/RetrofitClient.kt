9e40da38dbe584bb7db14178283075f509254401
fly_with24 <fly_with24@163.com>
2020-04-07 09:29:19 +0800

    提升各个依赖版本

--- app/src/main/java/com/yyz/wanandroid/data/api/RetrofitClient.kt
+++ app/src/main/java/com/yyz/wanandroid/data/api/RetrofitClient.kt
@@ -1,7 +1,6 @@
 package com.yyz.wanandroid.data.api
 
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
@@ -17,7 +16,6 @@ object RetrofitClient {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
         return@lazy retrofit.create<CommonService>()
     }
