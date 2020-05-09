9b0c9a765a13f226e771fb986358a095d14b6920
fly_with24 <fly_with24@163.com>
2020-05-09 16:29:13 +0800

    ArticleBoundaryCallback 逻辑

--- app/src/main/java/com/yyz/wanandroid/ui/home/tab/article/ArticleBoundaryCallback.kt
+++ app/src/main/java/com/yyz/wanandroid/ui/home/tab/article/ArticleBoundaryCallback.kt
@@ -1,7 +1,11 @@
 package com.yyz.wanandroid.ui.home.tab.article
 
+import android.util.Log
+import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.paging.PagedList
 import com.yyz.wanandroid.data.bean.Data
+import com.yyz.wanandroid.ui.home.tab.ArticleDao
+import kotlinx.coroutines.launch
 
 /**
  * @author yyz (杨云召)
@@ -9,16 +13,31 @@ import com.yyz.wanandroid.data.bean.Data
  * time   9:45
  * description
  */
-class ArticleBoundaryCallback : PagedList.BoundaryCallback<Data>() {
-    override fun onZeroItemsLoaded() {
+class ArticleBoundaryCallback(
+    private val lifecycleScope: LifecycleCoroutineScope,
+    private val dao: ArticleDao
+) : PagedList.BoundaryCallback<Data>() {
 
+    override fun onZeroItemsLoaded() {
+        lifecycleScope.launch {
+          /*  val result = Api.create().getHomeArticleList(0).data?.datas
+            if (result != null) {
+                dao.insertData(data = result)
+            }*/
+        }
     }
 
     override fun onItemAtEndLoaded(itemAtEnd: Data) {
-        super.onItemAtEndLoaded(itemAtEnd)
+       /* lifecycleScope.launch {
+            Log.i(TAG, "onItemAtEndLoaded: itemAtEnd ${itemAtEnd.indexInResponse} ")
+            val pageSize = (itemAtEnd.indexInResponse) / PAGE_SIZE
+            Log.i(TAG, "onItemAtEndLoaded: $pageSize")
+            val result =
+                Api.create().getHomeArticleList(pageSize).data?.datas
+            if (result != null) {
+                dao.insertData(data = result)
+            }
+        }*/
     }
 
-    override fun onItemAtFrontLoaded(itemAtFront: Data) {
-        super.onItemAtFrontLoaded(itemAtFront)
-    }
 }
