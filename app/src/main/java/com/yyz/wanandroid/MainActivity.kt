9c617d57d08c67c8c5059cf53e03e598dd2262b4
fly_with24 <fly_with24@163.com>
2020-05-11 17:08:37 +0800

    BottomNavigationView 平级保存状态

--- app/src/main/java/com/yyz/wanandroid/MainActivity.kt
+++ app/src/main/java/com/yyz/wanandroid/MainActivity.kt
@@ -1,26 +1,57 @@
 package com.yyz.wanandroid
 
-import androidx.navigation.findNavController
+
+import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.yyz.wanandroid.common.BaseActivity
+import com.yyz.wanandroid.common.setupWithNavController
 import com.yyz.wanandroid.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun initBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
-    override fun initView() {
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
+    override fun initView(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
+    }
+
+    private fun setupBottomNavigationBar() {
+        val navGraphIds = listOf(R.navigation.home, R.navigation.project, R.navigation.wx)
+
+        val controller = binding.navView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.navView.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home, R.id.project, R.id.wx)
+        )
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController, appBarConfiguration)
+        })
+        currentNavController = controller
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
